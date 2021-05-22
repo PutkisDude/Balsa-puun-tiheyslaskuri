568eab06d3c21cceffc30ac701f65436912250bc
PutkisDude <putkis@gmail.com>
2021-05-22 21:31:22 +0300

    Index about done

--- src/main/java/servlet/IndexServletti.java
+++ src/main/java/servlet/IndexServletti.java
@@ -1,8 +1,7 @@
 package servlet;
 
 import java.io.IOException;
-import java.time.LocalTime;
-import java.util.List;
+import java.text.DecimalFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import balsalaskin.Balsa;
-import balsalaskin.JDBCBalsaDao;
 
 @WebServlet("")
 public class IndexServletti extends HttpServlet {
@@ -19,10 +17,48 @@ public class IndexServletti extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    	JDBCBalsaDao dao = new JDBCBalsaDao();
-    	List<Balsa> balsalist = dao.haeKaikkiBalsat();
-        req.setAttribute("lista", balsalist);
+    	DecimalFormat desi = new DecimalFormat("0.00");
+    	// korkeus, pituus, leveys, paino
+    	String[] parametrit = new String[4];
+    	
+    	parametrit[0] = req.getParameter("paksuus");
+    	parametrit[1] = req.getParameter("pituus");
+    	parametrit[2] = req.getParameter("leveys");
+    	parametrit[3] = req.getParameter("paino");
+    	
+    	// Tarkistaa onko form täytetty
+    	boolean kaikkiTaytetty = true;
+    	
+    	for(String i : parametrit) {
+    		if(i == null) {
+    			kaikkiTaytetty = false;
+    			break;
+    		}
+    	}
+    	
+    	if(kaikkiTaytetty) {
+    		
+    		req.setAttribute("paksuus", parametrit[0]);
+    		req.setAttribute("pituus", parametrit[1]);
+    		req.setAttribute("leveys", parametrit[2]);
+    		req.setAttribute("paino", parametrit[3]);
 
+    			try{
+    				Double korkeus = Double.parseDouble(req.getParameter("paksuus")); 
+        			Double pituus = Double.parseDouble(req.getParameter("pituus"));
+        			Double leveys = Double.parseDouble(req.getParameter("leveys"));
+        			Double paino = Double.parseDouble(req.getParameter("paino"));    			
+        			 
+        			Balsa lasku = new Balsa(korkeus, leveys, pituus, paino);
+        			  
+        			String tuloste = desi.format(lasku.getTiheys()) + "kg/m3";
+        			req.setAttribute("tulos", tuloste);
+    			}catch(Exception e){
+    				req.setAttribute("tulos", "Tapahtui virhe.");
+    			}
+
+	}
+		 
         // lähetä request edelleen index.jsp sivulle
         req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
     }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,17 +1,41 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-    
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet" href="/styles/styles.css">
 <title>Puun tiheyslaskuri</title>
 </head>
 <body>
-<h1>Test</h1>
-<c:forEach items="${lista}" var="balsa">
-<p>${balsa}</p>
-</c:forEach>
+
+
+	<form method="get">
+	<fieldset><legend>Puun Tiheyslaskuri</legend>
+
+			<label for="paksuus">Paksuus(mm):</label>
+			<input type="text" name="paksuus" value="${paksuus}" required><br>
+		
+			<label for="pituus">Pituus(mm):</label> 
+			<input type="text" name="pituus" value="${pituus}" required><br>
+		
+			<label for="leveys">Leveys(mm):</label>
+			<input type="text" name="leveys" value="${leveys}" required><br>
+		
+			 <label for="paino">Paino(g):</label>
+		 	<input type="text" name="paino" value="${paino}" required><br>
+		 	<label for="tulos">Tulos: </label>
+		 	${tulos }
+		 	<input type="submit" value="Laske" id="laskubtn">
+		 	
+		</fieldset>
+				 	</form>
+		
+		<input type="submit" value="Premium version">
+		
+		
+		
 </body>
 </html>
